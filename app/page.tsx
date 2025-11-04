10cdca173843419d2b75693aa2f3bcbbed25b385
luay-naser <162501192+luay-naser@users.noreply.github.com>
2025-11-04 23:00:25 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-4xl">
+      <div className="mt-40 h-[1500px]">
+          helo
+        </div>
+    </div>
+  )
+}
