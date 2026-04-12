b513b19b4a92775f3ac3883c651ea79d0b9c43ac
luay-naser <162501192+luay-naser@users.noreply.github.com>
2026-04-12 15:29:19 +0300

    properties bug

--- app/api/propereties/route.ts
+++ app/api/propereties/route.ts
@@ -1,27 +1,14 @@
-import { NextResponse } from "next/server";
-
+// app/api/properties/route.ts
 export async function GET() {
-  try {
-    const res = await fetch(
-      "https://uninfectious-emilia-unmarshaled.ngrok-free.dev/project/real_estate/propreties.php"
-    );
+  const res = await fetch(
+    "https://uninfectious-emilia-unmarshaled.ngrok-free.dev/project/real_estate/propreties.php",
+    {
+      headers: {
+        "ngrok-skip-browser-warning": "true",
+      },
+    }
+  );
 
-    const text = await res.text();
-
-    console.log("RAW RESPONSE:");
-    console.log(text);
-
-    return new NextResponse(text, {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
-
-  } catch (error: any) {
-    console.log("SERVER ERROR:", error);
-
-    return NextResponse.json(
-      { error: error.message },
-      { status: 500 }
-    );
-  }
+  const data = await res.json();
+  return Response.json(data);
 }
